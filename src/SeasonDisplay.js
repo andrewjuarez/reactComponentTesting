cf0f8959dc1cd03d53b9dedb07c2bc71321ec764
andrewjuarez <a777juarez@gmail.com>
2019-02-22 22:54:03 -0800

    Finished function based comment component

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const SeasonDisplay = (props) => {
+    
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Comment from './Comment'
+import Comment from './Comment';
+import ApprovalCard from './ApprovalCard';
+import SeasonDisplay from './SeasonDisplay';
 import faker from 'faker'
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            console.log(position);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );
+    
     return (
-        <div className="ui container comments">
-            <Comment author='Andrew' timeAgo='Yesterday at 5:00PM' text='Cool story bro!' avatar={faker.image.avatar()} />
+        
+
+        <div className='ui container comments'>
+            <ApprovalCard>
+                <Comment author='Andrew' timeAgo='Yesterday at 5:00PM' text='Cool story bro!' avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment author='Bob' timeAgo='Yesterday at 5:00PM' text='I am hungry' avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment author='troll master' timeAgo='Yesterday at 5:00PM' text='HAHAHAHA' avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     )
 }
